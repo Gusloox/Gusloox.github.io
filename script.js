bcd04d5849c353a0855267ea2c48cf4c36b0952a
Konstantinos Spyropoulos <kostasmaniacgr@gmail.com>
2021-02-14 01:22:52 +0200

    Final commit for today, cya tomorrow:)

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const table = document.createElement("table");
 table.setAttribute("class", "table");
 document.querySelector(".container").appendChild(table);
 const Gameboard = (() => {
-    const gameboard = ["x", "0", "x", "0", "x", "0"];
+    const gameboard = [];
     return { gameboard };
 })();
 
@@ -16,29 +16,36 @@ const player2 = players("Player 2");
 player2.sayName()
 
 
+
 const displayController = (() => {
-    let gb = Array.from(Gameboard.gameboard);
+
+
     const createEmptyGrid = () => {
+        for (let row = 0; row < 3; row++) {
+            const tr = document.createElement("tr");
+            for (i = 0; i < 3; i++) {
+                const td = document.createElement("td");
+                td.setAttribute("onclick", "displayController.updateGrid()");
+                td.setAttribute("id", "item");
 
-        if (gb) {
 
-            for (let i = 0; i < 3; i++) {
+               
 
-                const tr = document.createElement("tr");
-                const td1 = document.createElement("td");
-                td1.innerHTML = gb[i];
-                const td2 = document.createElement("td");
-                td2.innerHTML = gb[i + 1];
-                const td3 = document.createElement("td");
-                td3.innerHTML = gb[i + 2];
-                table.appendChild(tr);
-                tr.appendChild(td1);
-                tr.appendChild(td2);
-                tr.appendChild(td3);
+                
+                tr.appendChild(td);
+
             }
+            table.appendChild(tr);
         }
     };
-    return{createEmptyGrid};
-})();
+    const updateGrid = (p) => {
+        Gameboard.gameboard.push('x');
+        let gb = Array.from(Gameboard.gameboard);
+        document.getElementById("item").innerText = gb[0];
 
+    };
+    return { createEmptyGrid, updateGrid };
+})();
 displayController.createEmptyGrid();
+
+
